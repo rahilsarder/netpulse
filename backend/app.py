a664576adb89bdd141628f935de4816e865c40a8
Sarder Monowarul Amin <sarder.amin.rahil@gmail.com>
2026-05-28 08:53:04 +0600

    websocket prod

--- backend/app.py
+++ backend/app.py
@@ -282,4 +282,10 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        debug=False,
+        allow_unsafe_werkzeug=True,
+    )
